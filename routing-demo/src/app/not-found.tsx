1e748b1b60a42ec24da3ce7e549273d40f31a5d4
Anwardeen Maideen Pichai <kalilanwar597@gmail.com>
2025-03-25 23:55:39 +0530

    working on the Not Found page

---
+++ routing-demo/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return(
+        <div>
+            <h2>Page Not Found!</h2>
+            <p>Requested Source isn't found</p>
+        </div>        
+    ) 
+}

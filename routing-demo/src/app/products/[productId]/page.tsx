7714bdd0d0f0d41a9966ae1a1421b3d21f290ba5
Anwardeen Maideen Pichai <kalilanwar597@gmail.com>
2025-04-22 00:33:01 +0530

    Learnt about the Metadata Object and generateMetadata function

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,9 +1,27 @@
-import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
-export default async function ProductDetails({params}:
-    {
-        params:Promise<{productId:string}>
-    }){
-        const productId = (await params).productId;
+type Props = {
+    params:Promise<{productId: string}>
+}
+
+export const generateMetadata = async({
+    params,
+}:Props):Promise<Metadata>=>{
+    const id = (await params).productId;
+    const title = await new Promise((resolve)=>{
+        setTimeout(()=>{
+         resolve(`Android ${id}`)   
+        },100)
+    })
+    return {
+        title:`Product ${title}`
+    }
+}
+
+export default async function ProductDetails({ params}: Props) {
+    const productId = (await params).productId;
     return <h1>Details of the Product {productId}</h1>
 }
+
+//Note: You can't use both generateMetadata and Metadata Object in the same routing!!!!!
+//And it won't work in the pages mentioned as "use client";

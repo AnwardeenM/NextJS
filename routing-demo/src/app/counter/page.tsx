4d95911b51bb882669ffbc0995ea821f15cea47d
Anwardeen Maideen Pichai <kalilanwar597@gmail.com>
2025-04-22 00:52:42 +0530

    Learnt about how to implement in Metadata with the Use client actions

---
+++ routing-demo/src/app/counter/page.tsx
@@ -0,0 +1,12 @@
+import {Counter} from "./counter";
+
+export const metadata={
+    title:"About the counter"
+}
+
+export default function CounterPage(){
+    return(
+        <Counter />
+    )
+
+}

e73954b4cb4c4bdbf67a2cece612ca9bf30de737
Anwardeen Maideen Pichai <kalilanwar597@gmail.com>
2025-04-23 07:40:30 +0530

    Learnt about the Navigation using UI with static and dynamic Linking

--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,10 +1,19 @@
+import Link from "next/link";
+
 export default function Products(){
+    const productId =99; 
     return (
         <>
+        <Link href="/">Home</Link>
         <h1>Product List</h1>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
+        <h2><Link href="/products/1">Product 1</Link></h2>
+        <h2><Link href="/products/2">Product 2</Link></h2>
+        <h2><Link href="/products/3" replace>Product 3</Link></h2>
+        <h2><Link href={`/products/${productId}`}>Product {productId}</Link></h2> 
         </>
     )
+
 }
+
+//This code contains the example of static and dynamic navigation using UI
+//replace is one of the features of next link, where you can write an navigation to different place.

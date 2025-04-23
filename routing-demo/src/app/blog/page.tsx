ee8db189a26143bda3908e1b7f288b6389ef873f
Anwardeen Maideen Pichai <kalilanwar597@gmail.com>
2025-04-23 07:24:05 +0530

    Learnt about the title Metadata

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,7 +1,9 @@
 import { Metadata } from "next";
 
 export const metadata:Metadata = {
-    title:"Blog"
+    title:{
+        absolute:"Blog"  //This gives the user to assign absolute title for the pages moving out of an assigned template.
+    }
 }
 
 export default function Blog(){
